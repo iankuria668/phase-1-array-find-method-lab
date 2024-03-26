9da4184f2bf0abfb1fe1cef42eeaeb171d5e32c5
iankuria668 <iankuria668@gmail.com>
2024-03-26 12:03:18 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const records = [
+    { year: "1978", result: "W"},
+    { year: "1977", result: "L"},
+    { year: "1976", result: "L"},
+    { year: "1975", result: "L"},
+    { year: "1974", result: "N/A"},
+    ]  
+
+const superbowlWin = function(record) {
+  for (let i = 0; i < record.length; i++) {
+    if (record[i].result === "W") {
+      return record[i].year
+    }
+  }
+}
